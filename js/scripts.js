a906fd7a9f91cc9bb30a7fc319de0a59c7228e88
Rich <rloke.nyc.rr@gmail.com>
2020-03-17 16:26:58 -0500

    V1 Pips

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+			
+
+		
+		function getNews() {
+			
+			document.getElementById("heading1").innerHTML = articles.title1;
+			document.getElementById("p1-1").innerHTML = articles.texts1[0].P1;
+			document.getElementById("p1-2").innerHTML = articles.texts1[1].P2;
+			
+		}
+
+
+	
